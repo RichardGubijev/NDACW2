699c8ae1caf2ad091f11b1c82d1024a21c6b4d1e
ElisabethH <elisabeth.halvorsen@kcl.ac.uk>
2024-04-12 11:01:49 +0100

    feat: make alignement meathod for

--- taskC/seeder.py
+++ taskC/seeder.py
@@ -1,54 +1,37 @@
-import math
-
 import geopandas as gpd
-from shapely.geometry import Point
 from sklearn.cluster import KMeans
-import networkx as nx
-import numpy as np
 import osmnx as ox
+import pickle
+import networkx as nx
 
-def get_seeds_kMeans(graph, num_seeds: int):
-    # 2. Extract node coordinates
-    coords = [(node, data['x'], data['y']) for node, data in graph.__nodes(data=True)]
+
+def get_seeds_with_kmeans(graph: nx.MultiDiGraph, num_seeds: int):
+    coords = [(node, data['x'], data['y']) for node, data in graph.nodes(data=True)]
     gdf_nodes = gpd.GeoDataFrame(coords, columns=['node_id', 'x', 'y'])
 
-    # 3. Apply K-means clustering
     kmeans = KMeans(n_clusters=num_seeds)
     kmeans.fit(gdf_nodes[['x', 'y']])
 
-    # 4. Extract cluster centroids as seeds
-    unaligned_seeds = [Point(x, y) for x, y in kmeans.cluster_centers_]
+    unaligned_seeds = [(x, y) for x, y in kmeans.cluster_centers_]
+    return __align_seeds(graph, unaligned_seeds)
 
-    # 5. Find closest nodes on the road network
-    closest_seeds = []
+
+def __align_seeds(graph: nx.MultiDiGraph, unaligned_seeds: list[tuple]) -> list:
+    nearest_node_ids = []
     for seed in unaligned_seeds:
-        distance, closest_node = nx.closest_node(graph, seed)  # Find distance along with the node
-        closest_point_on_road = nx.shortest_path(graph, seed, closest_node, weight='length')[1]
-        closest_seeds.append(Point(closest_point_on_road[0], closest_point_on_road[1]))
+        lat, lon = seed
+        nearest_node_ids.append(ox.nearest_nodes(graph, lat, lon, return_dist=False))
+    return nearest_node_ids
 
-    return closest_seeds
 
-# def eucledian_distance(point1, point2):
-#     return math.sqrt(point2**2)
-#
-# def closest_node(G, node, metric='euclidean'):
-#     """Find the node in graph G closest to 'node' based on a specified metric (default: Euclidean)"""
-#     distances = [(n, ) for n in G.nodes()]
-#     return min(distances, key=lambda x: x[1])[0]
+if __name__ == '__main__':
+    N_SEEDS = 10
 
-def get_nearest_edge(G, point):
-    # Get all edges in the graph
-    gdf = ox.graph_to_gdfs(G, nodes=False, fill_edge_geometry=True)
-    graph_edges = gdf[["geometry", "u", "v"]].values.tolist()
+    with open('../leeds_drive.pickle', 'rb') as f:
+        query_place_graph = pickle.load(f)
 
-    # Compute the Euclidean distance from the coordinates to each edge
-    edges_with_distances = [
-        (graph_edge, ox.Point(tuple(reversed(point))).distance(graph_edge[0]))
-        for graph_edge in graph_edges
-    ]
+    all_nodes = list(query_place_graph.nodes)
+    seeds = get_seeds_with_kmeans(query_place_graph, N_SEEDS)
 
-    # Sort edges based on distance
-    edges_with_distances.sort(key=lambda x: x[1])
 
-    # Return the closest edge (geometry and u, v nodes)
-    return edges_with_distances[0]
+
